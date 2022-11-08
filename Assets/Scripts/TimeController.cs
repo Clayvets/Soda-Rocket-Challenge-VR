2d857c2e11534620aa28b795cb5050902325c165
Clayvets <anamaria271127@gmail.com>
2022-11-08 02:21:02 -0500

    Contador de tiempo y WinCondition

---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class TimeController : MonoBehaviour
+{
+    public UnityEvent loseCondition;
+    
+    [SerializeField] private int min, seg;
+    [SerializeField] private Text tiempo;
+
+    private float restante;
+    private bool enMarchaEstoy;
+
+    private void Awake()
+    {
+        restante = (min + 60) + seg;
+        enMarchaEstoy = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enMarchaEstoy)
+        {
+            restante -= Time.deltaTime;
+            if (restante <= 0)
+            {
+                restante = 0;
+                enMarchaEstoy = false;
+                //SALE UI DE PERDER
+            }
+
+            int tempMin = (int)restante / 60;
+            int tempSeg = (int)restante % 60;
+            tiempo.text = tempMin.ToString("00") + ":" + tempSeg.ToString("00");
+        }
+        
+    }
+}
---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class WinCondition : MonoBehaviour
+{
+    public UnityEvent winCondition;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("BottleOfWater"))
+        {
+            if (winCondition != null)
+                winCondition.Invoke(); 
+            
+        }
+    }
+}
